60a5d45fa5e27106c9b714f44690461f7ad34ee1
panzhansheng <panzs@m.scnu.edu.cn>
2024-11-29 09:00:03 +0800

    Initial Commit

---
+++ GPUImage_demo/GPUImage_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPUImage_demoApp.swift
+//  GPUImage_demo
+//
+//  Created by zhansheng pan on 2024/11/29.
+//
+
+import SwiftUI
+
+@main
+struct GPUImage_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
